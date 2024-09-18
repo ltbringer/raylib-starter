8b0a302884ff4b791f6a41e3f0c1d573ddaf8785
ltbringer <amresh.venugopal@gmail.com>
2024-09-18 02:36:20 +0100

    update: ray window hello world.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,72 @@
-#include <stdio.h>
+# include "raylib.h"
+
 
 int main(void) {
-    printf("Hello, World!\n");
+    const char *message = "Hello World";
+    constexpr int fps = 60;
+    InitWindow(0, 0, message);
+
+    SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_VSYNC_HINT);
+
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
+    SetWindowSize(screenWidth, screenHeight);
+    SetTargetFPS(fps);
+
+    const auto backgroundColor = BLACK;
+    const auto textColor = WHITE;
+    const auto primaryButtonColor = GRAY;
+    const auto primaryInteractiveColor = DARKGRAY;
+    constexpr int buttonWidth = 150;
+    constexpr int buttonHeight = 50;
+    const Rectangle exitButton = {
+        (screenWidth - buttonWidth) / 2,
+        (screenHeight - buttonHeight) / 2,
+        buttonWidth,
+        buttonHeight
+    };
+    bool exitButtonHovered = false;
+
+    while (!WindowShouldClose()) {
+        const char *exitButtonText = "Exit";
+        const Vector2 exitButtonSize = MeasureTextEx(GetFontDefault(), exitButtonText, 20, 2);
+        constexpr int fontSize = 60;
+        exitButtonHovered = CheckCollisionPointRec(GetMousePosition(), exitButton);
+        if (exitButtonHovered && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            break;
+        }
+
+        BeginDrawing();
+        ClearBackground(backgroundColor);
+
+        const Vector2 textSize = MeasureTextEx(
+            GetFontDefault(),
+            message,
+            fontSize,
+            2
+        );
+        DrawText(
+            message,
+            (screenWidth - textSize.x) / 2,
+            screenHeight * 0.05,
+            fontSize,
+            textColor
+        );
+        DrawRectangleRec(
+            exitButton,
+            exitButtonHovered
+                ? primaryInteractiveColor
+                : primaryButtonColor
+        );
+        DrawText(exitButtonText,
+                 exitButton.x + (buttonWidth / 2) - (exitButtonSize.x / 2),
+                 exitButton.y + (buttonHeight / 2) - (exitButtonSize.y / 2),
+                 20,
+                 textColor
+        );
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
